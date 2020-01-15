94abbcc6901e1ff1b88420055ca7a50cb0c76b83
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-01-15 15:30:59 +0700

    Add files via upload

--- book.java
+++ book.java
@@ -10,25 +10,11 @@ public class book {
         int page = 0;
     }
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        String name = "Hello";
-        int coin = 5;
-        short myShort = 555;
-        long myLong = 1234;
-        double myDouble = 4444.444123123;
-        float myFloat = 1111.53f;
-        char myChar = 'S';
-        boolean myBoolean = false;
-        byte myByte = 24;
-        System.out.println(name);
-        System.out.println(coin);
-        System.out.println(myShort);
-        System.out.println(myLong);
-        System.out.println(myDouble);
-        System.out.println(myFloat);
-        System.out.println(myChar);
-        System.out.println(myBoolean);
-        System.out.println(myByte);
+       String greet = "Hello";
+       String blank = " ";
+       String name = "I'm Jo";
+       String total = greet + blank + name;
+       System.out.println(total);
     }
     
 }
