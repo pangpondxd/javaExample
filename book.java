69735e419a5cacd7457d88ff2e1211d115d47699
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-01-15 15:23:52 +0700

    Java type print

---
+++ book.java
@@ -0,0 +1,34 @@
+/**
+ * book
+ */
+public class book {
+    // public static book aboutBook(String name, int page) {
+    //     return 
+    // }
+    public book() {
+        String name = "Hello";
+        int page = 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        String name = "Hello";
+        int coin = 5;
+        short myShort = 555;
+        long myLong = 1234;
+        double myDouble = 4444.444123123;
+        float myFloat = 1111.53f;
+        char myChar = 'S';
+        boolean myBoolean = false;
+        byte myByte = 24;
+        System.out.println(name);
+        System.out.println(coin);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+    
+}
